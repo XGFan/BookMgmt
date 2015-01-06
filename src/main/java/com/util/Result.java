e1040c9a6d58de786dce02cab7ff64edc8490b03
Guofan <xgfan@vip.qq.com>
2015-01-06 18:45:39 +0800

    添加result类型

--- src/main/java/com/util/Result.java
+++ src/main/java/com/util/Result.java
@@ -1,22 +1,43 @@
 package com.util;
 
 /**
- * 结果？
+ * DATE:2014/12/2
+ * TIME:1:31
+ * Created by guofan on 2014/12/2
  */
+@SuppressWarnings("unused")
 public class Result {
-    private boolean result;
+    boolean status;
+    String msg;
 
-    public Result(boolean result) {
-        super();
-        this.result = result;
+    public Result() {
+        status = false;
+        msg = "";
     }
 
-    public boolean getResult() {
-        return result;
+    public Result(boolean status, String msg) {
+        this.status = status;
+        this.msg = msg;
     }
 
-    public void setResult(boolean result) {
-        this.result = result;
+    public boolean isStatus() {
+        return status;
     }
 
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String toJson() {
+        String json = "{" + "\"status\":\"" + status + "\"," + "\"msg\":\"" + msg + "\"}";
+        return json;
+    }
 }
